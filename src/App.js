4964475913c1c2b259f61cdd6ff02772f281f4b7
paolaagarmun <paola.a.garciamunoz@gmail.com>
2021-12-21 12:15:42 -0400

    added browser router in index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
+import BiographyView from './views/BiographyView';
+import HomeView from './views/HomeView';
+import WorksView from './views/WorksView';
 
 function App() {
   return (
-    <div className="App">
+    <>
     <NavBar/>
+    <Routes>
+      <Route path="/" element={<HomeView/>}/>
+      <Route path="/biography" element={<BiographyView/>}/>
+      <Route path="/works" element={<WorksView/>}/>
+    </Routes>
       let's build a website that shows Isabel Allende's life and works
-    </div>
+    </>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,20 @@
+
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import './NavBar.css'
 
 function NavBar () {
     return (
-        <div>
-            <ul className='navbar'>
-                <li>Home</li>
-                <li>Biography</li>
-                <li>Works</li>
-            </ul>
-        </div>
+        <Navbar bg="light" variant="light">
+            <Container>
+            <Nav className="me-auto">
+            <Link to="/">Home</Link>
+            <Link to="/biography">Biography</Link>
+            <Link to="/works">Works</Link>
+            </Nav>
+            </Container>
+        </Navbar>
     )
 }
 
-export default NavBar
+export default NavBar;
